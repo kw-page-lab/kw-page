424bd11e1a49c4cdfd6e5cb191e84802d5c61dec
MetaCodeX <102832240+MetaCodeX@users.noreply.github.com>
2026-06-06 06:42:31 +0000

    Deploying to gh-pages from @ kw-page-lab/kw-page@8e20189898bfd71d24cfa47fd9ee3be5c5d87370 🚀

--- src/js/ws-interceptor.js
+++ src/js/ws-interceptor.js
@@ -5,8 +5,40 @@
     
     const OriginalWebSocket = window.WebSocket;
     window.WebSocket = function(url, protocols) {
-        console.log('[WebSocket Interceptor] Intercepted WebSocket creation to:', url);
-        const ws = new OriginalWebSocket(url, protocols);
+        let modifiedUrl = url;
+        try {
+            let id = localStorage.getItem('kimeraware_client_id');
+            if (!id) {
+                id = 'client_' + Math.random().toString(36).substring(2, 15) + '_' + Date.now();
+                localStorage.setItem('kimeraware_client_id', id);
+            }
+            
+            let tempUrl = url;
+            let isWs = false;
+            let isWss = false;
+            if (url.startsWith('ws://')) {
+                tempUrl = url.replace('ws://', 'http://');
+                isWs = true;
+            } else if (url.startsWith('wss://')) {
+                tempUrl = url.replace('wss://', 'https://');
+                isWss = true;
+            }
+            
+            const urlObj = new URL(tempUrl, window.location.href);
+            urlObj.searchParams.set('clientId', id);
+            modifiedUrl = urlObj.toString();
+            
+            if (isWs) {
+                modifiedUrl = modifiedUrl.replace('http://', 'ws://');
+            } else if (isWss) {
+                modifiedUrl = modifiedUrl.replace('https://', 'wss://');
+            }
+        } catch(e) {
+            console.error('[WebSocket Interceptor] Failed to append clientId:', e);
+        }
+
+        console.log('[WebSocket Interceptor] Intercepted WebSocket creation to:', modifiedUrl);
+        const ws = new OriginalWebSocket(modifiedUrl, protocols);
         ws.addEventListener('open', () => {
             console.log('[WebSocket Interceptor] WebSocket connected!');
             window.wsConnected = true;
