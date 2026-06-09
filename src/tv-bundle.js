f58d1fadfe3a7316110796c9d565a4a5c913a465
MetaCodeX <102832240+MetaCodeX@users.noreply.github.com>
2026-06-09 01:23:21 +0000

    Deploying to gh-pages from @ kw-page-lab/kw-page@b4626f855b4cf9bcc87d18aadb9ae3dbdac1e702 🚀

--- src/tv-bundle.js
+++
@@ -1,8 +0,0 @@
-import { loadTV } from './tvModel.js';
-import { updateScreenManager } from './screenManager.js';
-
-// Expose functions globally to window
-window.KimerawareTV = {
-  loadTV,
-  updateScreenManager
-};
